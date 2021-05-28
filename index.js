b209a704a76bf019bd085bda81cbbb8926f0c338
Nethiri <marconaumann@t-online.de>
2021-05-28 15:16:42 +0200

    select

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const TextBoxArray = []
-const DivArray = [] 
+const DivArray = []
+let Selecttype = "Bild"
 let TheImage = document.createElement("img");
 
 // https://printjs.crabbly.com/ <------------------------------------------- PRINT
@@ -23,9 +24,22 @@ function Beginning() {
     HowToUse.innerHTML = "In order to use this tool. Please insert a Link to an Image you'd like to discribe below.";
     HowToUse.style = "display: block;text-align: center;line-height: 150%;font-size: 1em;"
     button.style ="width:90";
-    textBox.style ="width:210";
+    textBox.style ="width:160";
+    let select = document.createElement("select");
+    select.style ="width:50;height:22";
+    let opt1 = document.createElement("option");
+    let opt2 = document.createElement("option");
 
-
+    opt1.value = "Bild";
+    opt2.value = "Graph";
+    opt1.appendChild(document.createTextNode("Bild"));
+    opt2.appendChild(document.createTextNode("Graph"));
+    select.appendChild(opt1);
+    select.appendChild(opt2);
+    select.onchange = function() {
+        Selecttype = select.value
+    }
+    //
     button.innerHTML = "Load Image";
     button.addEventListener("click", function() {
         loadImage(textBox.value);
@@ -33,6 +47,7 @@ function Beginning() {
     div.appendChild(HowToUse);
     div.appendChild(textBox);
     div.appendChild(button);
+    div.appendChild(select)
     div.style = "position:fixed;z-index: 100;top:50%;left:47%;margin:-100px 0 0 -100px;width:300px;height:300px;"
     SetBody(div);
     return div;
@@ -46,8 +61,16 @@ function MidState() {
 
     //image discriber...
     let imgBeschreibung = document.createElement("DIV");
-    DiscribeMyImage(imgBeschreibung);
-    div.appendChild(imgBeschreibung);
+    
+    if(Selecttype === "Graph"){
+        DiscribeMyGraph(imgBeschreibung);
+        div.appendChild(imgBeschreibung);
+    }
+    else{    
+        DiscribeMyImage(imgBeschreibung);
+        div.appendChild(imgBeschreibung);
+    }
+
     SetBody(div);
 }
 
@@ -58,6 +81,20 @@ function DiscribeMyImage(location) {
     location.appendChild(createTextBox("Now we know what we see on the picture. Lets focus now on who is doing what or what is going on.<br><br>To take some examples:<br>The... is talking to...<br>... is guesturing to...<br>"));
     location.appendChild(createTextBox("So, that all is fine and dandy, lets now focus on what you are thinking... What do you think is the meaning of the picture?<br><br>Here are some examples:<br>It seems as if...<br>... might be a simbole of ... because...<br>I (don't) like ... because<br>... makes me think of ...<br>"));
 }
+
+function DiscribeMyGraph(location) {
+    location.appendChild(createTextBox("To capture the attention of your readers, you should start with a good introduction phrase(s).<br><br>Here are some examples you may use:<br>Let me show you this bar graph...<br>Let's turn to this diagram...<br>I'd like you to look at this map...<br>"));
+    location.appendChild(createTextBox("Now that we have the attention of your reader, and we have a general understanding of what has been displayed, you should start looking at details.<br><br>Here are some examples on how to continue:<br>The graph we are looking at has been published by...<br>The vertical axis shows...<br>Ther horizontal axis shows..."));
+    location.appendChild(createTextBox("Okay... we should now have finished the introduction of your discription.<br><br>Consider phrases like:<br>So, now please discribe what you see and the meaning of the informations displayed.<br>Each number are quivalent to 1 million...<br>The ... rises/increased/decreased/flattens...<br>The graph rapidly..."));
+    location.appendChild(createTextBox("Okay... lets bring that baby home guys. Let us finish with the conclusion, and we are done here!<br><br>Here are some examples to use:<br>Overall, we can see that...<br>In the end, you'll see...<br>From this we can conclude...<br>"));
+}
+//todo
+// code fragment discription?
+// ask zug?
+// 
+//
+//
+
 
 function loadImage(link) { // todo
     if(!link){
@@ -76,6 +113,7 @@ function createTextBox(beschreibung) {
     let ret = document.createElement("div");
     let beschreibungsfeld = document.createElement("lable");
     let textfeld = document.createElement("textarea");
+    textfeld.style = "width:100%"
     textfeld.onchange = function() {
         //todo
     }
