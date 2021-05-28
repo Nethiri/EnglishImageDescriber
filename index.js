8d97e88ee325592d3f32d93ccb3e66a624bcaf7a
Nethiri <marconaumann@t-online.de>
2021-05-28 12:05:49 +0200

    imageInsert

--- index.js
+++ index.js
@@ -1,15 +1,109 @@
 const TextBoxArray = []
+const DivArray = [] 
+let TheImage = document.createElement("img");
+
 // https://printjs.crabbly.com/ <------------------------------------------- PRINT
+onload = function() {
+    DivArray.push(Beginning());
+}
+function SetBody(div) {
+    RestBody();
+    document.getElementsByTagName("body")[0].appendChild(div);
+}
+function RestBody() {
+    document.getElementsByTagName("body")[0].innerHTML = "";
+}
+
+function Beginning() {
+    RestBody();
+    let div = document.createElement("DIV");
+    let button = document.createElement("button");
+    let textBox = document.createElement("input");
+    let HowToUse = document.createElement("label");
+    HowToUse.innerHTML = "In order to use this tool. Please insert a Link to an Image you'd like to discribe below.";
+    HowToUse.style = "display: block;text-align: center;line-height: 150%;font-size: 1em;"
+    button.style ="width:90";
+    textBox.style ="width:210";
 
 
-function loadImage() {
-    let place = document.getElementById("placeholder");
-    place.innerHTML = ""
-    let link = document.getElementById("textInputfield").value;
-    //alert(link);
-    let myImage = document.createElement("img");
-    myImage.src = link;
-    place.appendChild(myImage);
+    button.innerHTML = "Load Image";
+    button.addEventListener("click", function() {
+        loadImage(textBox.value);
+    });
+    div.appendChild(HowToUse);
+    div.appendChild(textBox);
+    div.appendChild(button);
+    div.style = "position:fixed;z-index: 100;top:50%;left:47%;margin:-100px 0 0 -100px;width:300px;height:300px;"
+    SetBody(div);
+    return div;
+}
+
+function MidState() {
+    RestBody();
+    let div = document.createElement("DIV");
+    TheImage.style = "display: block; margin-left: auto; margin-right: auto; width: 50%;"
+    div.appendChild(TheImage);
+
+    //image discriber...
+    let imgBeschreibung = document.createElement("DIV");
+    DiscribeMyImage(imgBeschreibung);
+    div.appendChild(imgBeschreibung);
+    SetBody(div);
+}
+
+function DiscribeMyImage(location) {
+    let ret = document.createElement("div");
+    
+
+
+
+
+
+
+
+
+
+    location = ret;
+    return ret;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function loadImage(link) { // todo
+    if(!link){
+        alert("Please insert a Link into the Textbox to proceed.");
+        return;
+    }    
+    try{TheImage.src = link;}
+    catch(error) {
+        alert("Something wasnt Okay with that Image Link! Make sure it's a Link to an Image!");
+    }
+    // bla bla, if successful, go into next state
+    MidState();
 }
 
 function createTextBox(beschreibung) {
